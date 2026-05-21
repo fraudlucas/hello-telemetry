fb5a227b2cb49a98285b466b628082d407ec0c22
Marcus Lucas <marcus.falcao@serpro.gov.br>
2026-05-21 20:12:28 -0300

    Add manual code-based instrumentation in the Python service

--- python-service/compute.py
+++ python-service/compute.py
@@ -1,9 +1,53 @@
 from flask import Flask, request, jsonify
 
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+
+
+APP_SERVICE_NAME = "ht-python-service"
+OTEL_ATTRIBUTE_SERVICE_NAME = "service.name"
+OTLP_GRPC_EXPORTER_ENDPOINT = "http://ht-otel-collector:4317"
+OTEL_METER_INTERVAL_SECONDS = 10000
+OTEL_INSTRUMENTATION_SCOPE_NAME = __name__
+OTEL_CUSTOM_METRIC_NAME = "app_compute_request_count"
+OTEL_CUSTOM_METRIC_DESCRIPTION = "Counts the requests to compute-service"
+
+
+otel_resource = Resource.create({OTEL_ATTRIBUTE_SERVICE_NAME: APP_SERVICE_NAME})
+
+# Metrics setup: exporter, metric reader, meter provider
+otlp_metric_exporter = OTLPMetricExporter(
+    endpoint=OTLP_GRPC_EXPORTER_ENDPOINT, insecure=True
+)
+
+otel_metric_reader = PeriodicExportingMetricReader(
+    exporter=otlp_metric_exporter, export_interval_millis=OTEL_METER_INTERVAL_SECONDS
+)
+
+otel_meter_provider = MeterProvider(
+    resource=otel_resource, metric_readers=(otel_metric_reader,)
+)
+
+metrics.set_meter_provider(otel_meter_provider)
+
+otel_meter = metrics.get_meter(OTEL_INSTRUMENTATION_SCOPE_NAME)
+
+otel_compute_request_count = otel_meter.create_counter(
+    name=OTEL_CUSTOM_METRIC_NAME,
+    description=OTEL_CUSTOM_METRIC_DESCRIPTION,
+)
+
+
 app = Flask(__name__)
 
 @app.route('/compute_average_age', methods=['POST'])
 def compute_average_age():  
+
+    # Increment compute_request_count
+    otel_compute_request_count.add(1)
 
     # Process the request data
     data = request.json['data']
