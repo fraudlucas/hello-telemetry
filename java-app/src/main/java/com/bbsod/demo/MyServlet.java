64762a361e1d020907322c4b73bb41a35228f447
Marcus Lucas <marcus.falcao@serpro.gov.br>
2026-05-27 14:04:14 -0300

    Remove the opentelemetry init config in the Java service. Adjust the code to use the zero code agent instrumentation.

--- java-app/src/main/java/com/bbsod/demo/MyServlet.java
+++ java-app/src/main/java/com/bbsod/demo/MyServlet.java
@@ -7,7 +7,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -21,42 +20,20 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.Context;
-import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
-import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.logs.SdkLoggerProvider;
-import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
-import io.opentelemetry.sdk.metrics.SdkMeterProvider;
-import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class MyServlet extends HttpServlet {
-
-    private static final String OTEL_ATTRIBUTE_SERVICE_NAME = "service.name";
-
-    private static final String SERVICE_NAME = "ht-tomcat-service";
-
-    private static final String OTLP_GRPC_EXPORTER_ENDPOINT = "http://ht-otel-collector:4317";
-
-    private static final Long OTEL_METER_INTERVAL_SECONDS = 10L;
 
     private static final String INSTRUMENTATION_SCOPE_NAME = MyServlet.class.getName();
     private static final String METRIC_NAME = "app.db.db_requests"; // Look the semantic convention to avoid conflict.
@@ -65,14 +42,12 @@ public class MyServlet extends HttpServlet {
     private final Meter otelMeter;
     private final LongCounter requestCounter;
     private final Tracer otelTracer;
-    private static final java.util.logging.Logger julLogger = Logger.getLogger("jul-logger") ;
-
-    private Context otelParentContext;
+    private static final java.util.logging.Logger julLogger = Logger.getLogger("jul-logger");
 
     // Constructor
     public MyServlet() {
 
-        OpenTelemetry otel = initOpenTelemetry();
+        OpenTelemetry otel = GlobalOpenTelemetry.get();
 
         this.otelMeter = otel.getMeter(INSTRUMENTATION_SCOPE_NAME);
 
@@ -93,69 +68,6 @@ public class MyServlet extends HttpServlet {
 
     }
 
-    private static OpenTelemetry initOpenTelemetry() {
-
-        // Resource service.name setup
-        Attributes otelAttributes = Attributes.of(
-                AttributeKey.stringKey(OTEL_ATTRIBUTE_SERVICE_NAME), SERVICE_NAME);
-
-        Resource otelResource = Resource.create(otelAttributes);
-
-        /* CUSTOM METRIC SETUP */
-
-        // OTLP Exporter setup
-        OtlpGrpcMetricExporter otlpGrpcMetricExporter = OtlpGrpcMetricExporter.builder()
-                .setEndpoint(OTLP_GRPC_EXPORTER_ENDPOINT)
-                .build();
-
-        // Metric reader setup
-        PeriodicMetricReader otelPeriodicMetricReader = PeriodicMetricReader.builder(otlpGrpcMetricExporter)
-                .setInterval(Duration.ofSeconds(OTEL_METER_INTERVAL_SECONDS))
-                .build();
-
-        // Otel Meter Provider setup
-        SdkMeterProvider otelSdkMeterProvider = SdkMeterProvider.builder()
-                .addResource(otelResource)
-                .registerMetricReader(otelPeriodicMetricReader)
-                .build();
-
-        /* CUSTOM SPAN SETUP */
-
-        OtlpGrpcSpanExporter otlpGrpcSpanExporter = OtlpGrpcSpanExporter.builder()
-                .setEndpoint(OTLP_GRPC_EXPORTER_ENDPOINT)
-                .build();
-
-        SimpleSpanProcessor otelSimpleSpanProcessor = SimpleSpanProcessor.builder(otlpGrpcSpanExporter).build();
-
-        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-                .addResource(otelResource)
-                .addSpanProcessor(otelSimpleSpanProcessor)
-                .build();
-
-        /* LOGs SETUP */
-        OtlpGrpcLogRecordExporter otlpGrpcLogRecordExporter = OtlpGrpcLogRecordExporter.builder()
-                .setEndpoint(OTLP_GRPC_EXPORTER_ENDPOINT)
-                .build();
-
-        BatchLogRecordProcessor otelBatchLogRecordProcessor = BatchLogRecordProcessor.builder(otlpGrpcLogRecordExporter)
-                .build();
-
-        SdkLoggerProvider otelSdkLoggerProvider = SdkLoggerProvider.builder().setResource(otelResource)
-                .addLogRecordProcessor(otelBatchLogRecordProcessor).build();
-
-        // Otel SDK setup.
-        OpenTelemetrySdk otelSdk = OpenTelemetrySdk.builder()
-                .setMeterProvider(otelSdkMeterProvider)
-                .setTracerProvider(sdkTracerProvider)
-                .setLoggerProvider(otelSdkLoggerProvider)
-                .build();
-
-        // Cleanup
-        Runtime.getRuntime().addShutdownHook(new Thread(otelSdk::close));
-
-        return otelSdk; // OpenTelemetrySdk implements OpenTelemetry interface.
-    }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -164,43 +76,28 @@ public class MyServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
 
-        // Parent span for context propagation setup
-        Span otelDoGetSpan = this.otelTracer.spanBuilder("Do Get")
-                .setNoParent()
-                .startSpan();
+        // Sleep for 2 seconds
+        sleepFor(2000);
 
-        try {
-            otelDoGetSpan.makeCurrent();
+        // Establish database connection and get data
+        this.requestCounter.add(1);
 
-            this.otelParentContext = Context.current().with(otelDoGetSpan);
+        out.println("<html><body>");
+        out.println("<h1>Database Results</h1>");
 
-            // Sleep for 2 seconds
-            sleepFor(2000);
+        getDatabaseResults(dataList, out);
 
-            // Establish database connection and get data
-            this.requestCounter.add(1);
+        // Make a request to the Python microservice
+        String averageAge = getAverageAge(dataList);
 
-            out.println("<html><body>");
-            out.println("<h1>Database Results</h1>");
-
-            getDatabaseResults(dataList, out);
-
-            // Make a request to the Python microservice
-            String averageAge = getAverageAge(dataList);
-
-            out.println("<h2>Average Age: " + averageAge + "</h2>");
-            out.println("</body></html>");
-
-        } finally {
-            otelDoGetSpan.end();
-        }
+        out.println("<h2>Average Age: " + averageAge + "</h2>");
+        out.println("</body></html>");
 
     }
 
     private void sleepFor(long millis) {
         Span otelSleepSpan = this.otelTracer.spanBuilder("Sleep for two seconds")
                 .setSpanKind(SpanKind.INTERNAL)
-                .setParent(this.otelParentContext)
                 .startSpan();
 
         try {
@@ -215,7 +112,6 @@ public class MyServlet extends HttpServlet {
     private void getDatabaseResults(List<JSONObject> dataList, PrintWriter out) {
         Span otelDbSpan = this.otelTracer.spanBuilder("Database Connection")
                 .setSpanKind(SpanKind.CLIENT)
-                .setParent(this.otelParentContext)
                 .startSpan();
 
         // JDBC connection parameters
@@ -277,10 +173,7 @@ public class MyServlet extends HttpServlet {
 
         Span otelAverageSpan = this.otelTracer.spanBuilder("Compute Average Age")
                 .setSpanKind(SpanKind.CLIENT)
-                .setParent(otelParentContext)
                 .startSpan();
-
-        Context otelAverageContext = Context.current().with(otelAverageSpan);
 
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpPost httpPost = new HttpPost("http://ht-python-service:5000/compute_average_age");
@@ -291,10 +184,6 @@ public class MyServlet extends HttpServlet {
 
             StringEntity entity = new StringEntity(requestData.toString());
             httpPost.setEntity(entity);
-
-            // W3CTraceContext for span context propagation
-            W3CTraceContextPropagator traceContextPropagator = W3CTraceContextPropagator.getInstance();
-            traceContextPropagator.inject(otelAverageContext, httpPost, HttpPost::setHeader);
 
             String responseString = httpClient.execute(httpPost,
                     response -> EntityUtils.toString(response.getEntity()));
