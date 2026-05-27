aa933411f1abe22ba59f0ea032a7a339bbc9f1dc
Marcus Lucas <marcus.falcao@serpro.gov.br>
2026-05-27 14:25:22 -0300

    Remove the opentelemetry init config in the Python service. Adjust the code to use the zero code agent instrumentation.

--- python-service/compute.py
+++ python-service/compute.py
@@ -1,25 +1,12 @@
 from flask import Flask, request, jsonify
 
 from opentelemetry import metrics, trace
-from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
-from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
-from opentelemetry._logs import set_logger_provider
 
 import logging
 
 APP_SERVICE_NAME = "ht-python-service"
 OTEL_ATTRIBUTE_SERVICE_NAME = "service.name"
-OTLP_GRPC_EXPORTER_ENDPOINT = "http://ht-otel-collector:4317"
-OTEL_METER_INTERVAL_SECONDS = 10000
 OTEL_INSTRUMENTATION_SCOPE_NAME = __name__
 OTEL_CUSTOM_METRIC_NAME = "app_compute_request_count"
 OTEL_CUSTOM_METRIC_DESCRIPTION = "Counts the requests to compute-service"
@@ -27,21 +14,7 @@ OTEL_CUSTOM_METRIC_DESCRIPTION = "Counts the requests to compute-service"
 
 otel_resource = Resource.create({OTEL_ATTRIBUTE_SERVICE_NAME: APP_SERVICE_NAME})
 
-# Metrics setup: exporter, metric reader, meter provider
-
-otlp_metric_exporter = OTLPMetricExporter(
-    endpoint=OTLP_GRPC_EXPORTER_ENDPOINT, insecure=True
-)
-
-otel_metric_reader = PeriodicExportingMetricReader(
-    exporter=otlp_metric_exporter, export_interval_millis=OTEL_METER_INTERVAL_SECONDS
-)
-
-otel_meter_provider = MeterProvider(
-    resource=otel_resource, metric_readers=(otel_metric_reader,)
-)
-
-metrics.set_meter_provider(otel_meter_provider)
+# Metrics setup:
 
 otel_meter = metrics.get_meter(OTEL_INSTRUMENTATION_SCOPE_NAME)
 
@@ -50,43 +23,14 @@ otel_compute_request_count = otel_meter.create_counter(
     description=OTEL_CUSTOM_METRIC_DESCRIPTION,
 )
 
-# Traces setup: span exporter, span processor, tracer provider
-
-otlp_span_exporter = OTLPSpanExporter(
-    endpoint=OTLP_GRPC_EXPORTER_ENDPOINT, insecure=True
-)
-
-otel_span_processor = BatchSpanProcessor(otlp_span_exporter)
-
-otel_tracer_provider = TracerProvider(resource=otel_resource)
-otel_tracer_provider.add_span_processor(otel_span_processor)
-
-trace.set_tracer_provider(otel_tracer_provider)
+# Tracer setup:
 
 otel_tracer = trace.get_tracer(OTEL_INSTRUMENTATION_SCOPE_NAME)
 
-# Logs setup: exporter, processor, provider, handler
+# Logs setup:
 
-otlp_logs_exporter = OTLPLogExporter(
-    endpoint=OTLP_GRPC_EXPORTER_ENDPOINT, insecure=True
-)
-
-otel_logs_processor = BatchLogRecordProcessor(exporter=otlp_logs_exporter)
-
-otel_logs_provider = LoggerProvider(resource=otel_resource)
-otel_logs_provider.add_log_record_processor(otel_logs_processor)
-
-set_logger_provider(otel_logs_provider)
-
-otel_logs_handler = LoggingHandler(
-    level=logging.NOTSET, logger_provider=otel_logs_provider
-)
-
-# Logging and logger setup
-
-logging.basicConfig(level=logging.NOTSET, handlers=[otel_logs_handler])
-
-otel_logger = logging.getLogger()
+otel_logger = logging.getLogger(OTEL_INSTRUMENTATION_SCOPE_NAME)
+otel_logger.setLevel(logging.INFO)
 
 
 app = Flask(__name__)
@@ -95,13 +39,8 @@ app = Flask(__name__)
 @app.route("/compute_average_age", methods=["POST"])
 def compute_average_age():
 
-    # Extract Otel trace context
-    otel_trace_context = TraceContextTextMapPropagator().extract(request.headers)
-
     # Starting a new span
-    with otel_tracer.start_as_current_span(
-        name="Compute Average Span", context=otel_trace_context
-    ):
+    with otel_tracer.start_as_current_span(name="Compute Average Span"):
 
         otel_logger.info("Average age compute in progress")
 
